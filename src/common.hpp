bb685c6681446d749303f84345a978f456cf2f7c
Nino Maruszewski <nino.maruszewski@gmail.com>
2023-08-02 10:36:07 -0500

    refactor: extract frame into its own file

--- src/common.hpp
+++ src/common.hpp
@@ -1,4 +1,7 @@
 #include "config.h"
 #include "logging.hpp"
 
+#include <fmt/format.h>
+#include <fmt/xchar.h>
+
 #define UNUSED(x) (void)(x)
--- src/gui/gui.cpp
+++ src/gui/gui.cpp
@@ -1,9 +1,9 @@
 #include "gui.hpp"
 
 #include "common.hpp"
-#include "gui/constants.hpp"
-#include "gui/utils.hpp"
+#include "frames/frames.hpp"
 #include "logging.hpp"
+#include "utils.hpp"
 
 #include <wx/wxprec.h>
 
@@ -12,96 +12,12 @@
 #  include <wx/wx.h>
 #endif
 
-// Make sure we have everything enabled in wx that we need
-static_assert(wxUSE_MENUBAR && wxUSE_STATUSBAR, "Missing required wxWidgets features");
-
 namespace krompir {
 namespace gui {
 
-// ----------------------------------------------------------------------------
-
-// Define a new frame type: this is going to be our main frame
-class MainFrame : public wxFrame {
-    /**
-     * Create the menu bar for this frame and set it.
-     */
-    void
-    create_menu_bar_()
-    {
-        // Create our "File" menu
-        auto* file_menu = new wxMenu();
-        file_menu->Append(CONTROL_QUIT, "E&xit\tAlt-X", "Quit this program");
-
-        // the "About" item should be in the help menu
-        auto* help_menu = new wxMenu();
-        help_menu->Append(CONTROL_ABOUT, "&About\tF1", "Show about dialog");
-
-        // now append the freshly created menu to the menu bar...
-        auto* menu_bar = new wxMenuBar();
-        menu_bar->Append(file_menu, "&File");
-        menu_bar->Append(help_menu, "&Help");
-
-        // ... and attach this menu bar to the frame
-        SetMenuBar(menu_bar);
-    }
-
-public:
-    // ctor(s)
-    explicit MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title)
-    {
-        // set the frame icon
-        SetIcon(wxICON(sample));
-
-        // create a menu bar
-        create_menu_bar_();
-
-        // create a status bar just for fun (by default with 1 pane only)
-        CreateStatusBar(2);
-        SetStatusText("Welcome to Krompir!");
-
-        // And bind events
-        Bind(
-            wxEVT_MENU, [this](wxCommandEvent&) { Close(true); }, CONTROL_QUIT
-        );
-        Bind(wxEVT_MENU, &MainFrame::on_about_, this, CONTROL_ABOUT);
-    }
-
-private:
-    /*
-     * event handlers (these functions should _not_ be virtual or static)
-     */
-
-    /**
-     * Called when the about button is pressed or an about command is sent to the
-     * window.
-     */
-    void
-    on_about_(wxCommandEvent& event)
-    {
-        UNUSED(event);
-
-        wxMessageBox(
-            wxString::Format(
-                "Welcome to Krompir!\n"
-                "\n"
-                "Application Information:\n"
-                " - %s\n"
-                " - Binlog @ %s\n"
-                " - %s\n",
-                wxVERSION_STRING,
-                BINLOG_VERSION,
-                wxGetOsDescription()
-            ),
-            "About Krompir",
-            wxOK | wxICON_INFORMATION, // NOLINT(hicpp-signed-bitwise)
-            this
-        );
-    }
-};
-
-// ----------------------------------------------------------------------------
-
-// Define the main application
+/**
+ * @brief The application class, which defines the entire wxWidgets application.
+ */
 class KrompirApp : public wxApp {
 public:
     // override base class virtuals
